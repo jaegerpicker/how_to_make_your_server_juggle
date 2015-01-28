524c5e3047c1ca2e276e7b261c064d6830e07a82
Shawn Campbell <scampbell@cashstar.com>
2015-01-28 18:46:32 -0500

    basic go server

--- csp_go/main.go
+++ csp_go/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	_ "sync"
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"net"
+	"strings"
+	_ "sync"
 	"time"
 )
 
-type player_move struct {
-	position        position
-	player   		player
+type playerMove struct {
+	position position
+	player   player
 }
 
 type player struct {
-	position_at		position
-	player_name     string
-    player_number   int
-    alive           bool
-    num_turns       int
-    power           int
+	positionAt   position
+	playerName   string
+	playerNumber int
+	alive        bool
+	numTurns     int
+	power        int
 }
 
 type position struct {
@@ -27,22 +32,22 @@ type position struct {
 }
 
 type board struct {
-	rows	[]int
-	columns	[]string
+	rows    []int
+	columns []string
 }
-var board_x int = 5
-var board_y int = 5
-var playing_board = board{
-	rows: make([]int, board_x),
-	columns: make([]string, board_y),
-}
-var write_to_board = make(chan player_move, 0)
-var random_positions bool = true
-var number_of_players int = 4
-var players = make([]player, number_of_players)
-var player_name_length int = 5
-var letters = []rune("abcdefghijklmnopqrstuvwxyz")
 
+var boardX = 5
+var boardY = 5
+var playingBoard = board{
+	rows:    make([]int, boardX),
+	columns: make([]string, boardY),
+}
+var writeToBoard = make(chan playerMove, 0)
+var randomPositions = true
+var numberOfPlayers = 4
+var players = make([]player, numberOfPlayers)
+var playerNameLength = 5
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
 
 func randSeq(n int) string {
 	b := make([]rune, n)
@@ -54,103 +59,135 @@ func randSeq(n int) string {
 
 func playerInPlayerList(playerlist []player, player string) bool {
 	for _, b := range playerlist {
-		if b.player_name == player {
+		if b.playerName == player {
 			return true
 		}
 	}
 	return false
 }
 
-func createNewPlayer(players_list []player) string {
-	new_player := randSeq(player_name_length)
-	for playerInPlayerList(players_list, new_player) {
-		new_player = createNewPlayer(players_list)
+func createNewPlayer(playersList []player) string {
+	newPlayer := randSeq(playerNameLength)
+	for playerInPlayerList(playersList, newPlayer) {
+		newPlayer = createNewPlayer(playersList)
 	}
-	return new_player
+	return newPlayer
 }
 
 func setup() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	for b := 0; b < board_y; b++ {
-		playing_board.columns[b] = string(letters[b])
+	for b := 0; b < boardY; b++ {
+		playingBoard.columns[b] = string(letters[b])
 	}
-	for r := 0; r < board_x; r++ {
-		playing_board.rows[r] = r
+	for r := 0; r < boardX; r++ {
+		playingBoard.rows[r] = r
 	}
-	for i :=0; i < number_of_players; i++ {
-		new_player_name := createNewPlayer(players)
-		rand_column := rand.Intn(board_y)
-		rand_row := rand.Intn(board_x)
-		new_player := player {
-			position_at:  position {row: playing_board.rows[rand_row], column: playing_board.columns[rand_column]},
-			player_name: new_player_name,
-			player_number: i,
-			alive:	true,
-			num_turns: 0,
-			power: rand.Intn(number_of_players),
+	for i := 0; i < numberOfPlayers; i++ {
+		newPlayerName := createNewPlayer(players)
+		randColumn := rand.Intn(boardY)
+		randRow := rand.Intn(boardX)
+		newPlayer := player{
+			positionAt:   position{row: playingBoard.rows[randRow], column: playingBoard.columns[randColumn]},
+			playerName:   newPlayerName,
+			playerNumber: i,
+			alive:        true,
+			numTurns:     0,
+			power:        rand.Intn(numberOfPlayers),
 		}
-		players[i] = new_player
+		players[i] = newPlayer
 	}
 	fmt.Println(fmt.Sprintf("Welcome to the %v deathmatch! \nLet's greet our players", time.Now()))
-	for _,i := range players {
-		fmt.Println(fmt.Sprintf("Player %s welcome!", i.player_name))
+	for _, i := range players {
+		fmt.Println(fmt.Sprintf("Player %s welcome!", i.playerName))
 	}
 }
 
 func printBoardState() {
-	player_there := false
+	playerThere := false
 	label := ""
 	fmt.Println("Board state:")
 	fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-	for i := 0; i < board_y; i++ {
-		for j:= 0; j < board_x; j++ {
+	for i := 0; i < boardY; i++ {
+		for j := 0; j < boardX; j++ {
 			for _, p := range players {
-				if playing_board.columns[i] == p.position_at.column && playing_board.rows[j] == p.position_at.row {
-					player_there = true
-					label = p.player_name
+				if playingBoard.columns[i] == p.positionAt.column && playingBoard.rows[j] == p.positionAt.row {
+					playerThere = true
+					label = p.playerName
 				}
 			}
-			if !player_there {
-				label = playing_board.columns[i] + fmt.Sprintf("%d", playing_board.rows[j])
+			if !playerThere {
+				label = playingBoard.columns[i] + fmt.Sprintf("%d", playingBoard.rows[j])
 			}
 			fmt.Printf(" | %5s | ", label)
-			player_there = false
+			playerThere = false
 		}
 		fmt.Println("")
 	}
 	fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 }
 
-func playersInBattle(p1 player, p2 player) (bool) {
-	if p2.player_name != p.player_name && p2.position_at.row == p.position_at.row && p2.position_at.column == p.position_at.column {
-		true
-	} else {
-		false
+func playersInBattle(p1 player, p2 player) bool {
+	if p2.playerName != p1.playerName && p2.positionAt.row == p1.positionAt.row && p2.positionAt.column == p1.positionAt.column {
+		return true
 	}
+	return false
 }
 
 func playerResolver() {
-	p <- write_to_board
-	num_of_players_alive := number_of_players
+	var pm playerMove
+	var p player
+	pm = <-writeToBoard
+	p = pm.player
+	numOfPlayersAlive := numberOfPlayers
 	for i, p2 := range players {
-		if p2.player_name == p.player_name {
+		if p2.playerName == p.playerName {
 			players[i] = p
 		}
 		if playersInBattle(p, p2) {
 			if p.power > p2.power {
 				p2.alive = false
-				num_of_players_alive--
+				numOfPlayersAlive--
 			} else {
-				p.power = false
-				num_of_players_alive--
+				p.alive = false
+				numOfPlayersAlive--
 			}
 		}
 	}
 	printBoardState()
 }
 
+func handleConnection(conn net.Conn) {
+	connbuf := bufio.NewReader(conn)
+	for {
+		str, err := connbuf.ReadString('\n')
+		if len(str) > 0 {
+			if strings.Trim(str, "\n") == "exit" {
+				break
+			}
+			fmt.Println(str)
+		}
+		if err != nil {
+			break
+		}
+	}
+	io.Copy(conn, conn)
+	conn.Close()
+}
+
 func main() {
 	setup()
 	printBoardState()
 	go playerResolver()
+
+	ln, err := net.Listen("tcp", ":9393")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn)
+	}
 }
