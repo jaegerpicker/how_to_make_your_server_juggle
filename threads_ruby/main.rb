e21c70f1bb9d7877fd3af8e49207109b644dd713
Shawn Campbell <scampbell@cashstar.com>
2015-01-28 21:38:42 -0500

    some work on the ruby theards example

--- threads_ruby/main.rb
+++ threads_ruby/main.rb
@@ -1,1 +1,23 @@
-#! /bin/ruby
+#! /usr/bin/ruby
+
+class player
+    def initialize(position_at, player_name, player_number, alive, num_turns, power)
+        @position_at = position_at
+        @player_name = player_name
+        @player_number = player_number
+        @alive = alive
+        @num_turns = num_turns
+        @power = power
+    end
+end
+
+class position
+    def initialize(row, column)
+        @row = row
+        @column = column
+    end
+end
+
+def setup
+
+end
