ce57876b50349036dba2ebb228d900f1d520d67c
Shawn Campbell <scampbell@cashstar.com>
2015-02-10 23:11:47 -0500

    finished up ruby threads sample

--- threads_ruby/main.rb
+++ threads_ruby/main.rb
@@ -2,7 +2,7 @@
 require './board.rb'
 require './player.rb'
 require 'thread'
-
+require 'socket'
 
 class Position
   attr_accessor :row
@@ -21,6 +21,8 @@ class Game
   attr_accessor :number_of_players
   attr_accessor :players
   attr_accessor :players_name_length
+  attr_accessor :player_move
+
   def print_board_state 
     player_there = false
     label = ""
@@ -45,10 +47,11 @@ class Game
   end
 
   def increase_column(column)
-    if column.next in @playing_board.columns
+    if @playing_board.columns.include? column.next
       return column.next
     else
       return column
+    end
   end
 
   def decrease_column(column)
@@ -78,10 +81,10 @@ class Game
           if players_in_battle(p, p2)
             if p.power > p2.power
               p2.alive = false
-              number_of_players_alive--
+              number_of_players_alive -= 1
             else
               p.alive = false
-              number_of_players_alive--
+              number_of_players_alive -= 1
             end
           end
         end
@@ -98,6 +101,7 @@ class Game
     @number_of_players = 4
     @players = [@number_of_players]
     @players_name_length = 5
+    @player_move = {"player" => Player.new, "position" => [], "new_move" => false}
     letters = [*"A".."Z"]                    
     (0..(@board_y -1)).each do |i|
       @playing_board.columns[i] = letters[i]
@@ -118,6 +122,68 @@ class Game
     end
     print_board_state()
   end
+
+  def run_game
+    semaphore = Mutex.new
+    resolver = Thread.new 
+      loop do
+        if @player_move['new_move']
+          semaphore.synchronize 
+            @players.each do |p|
+              if p.player_name == @player_move['player'].player_name
+                p.position = @player_move['position']
+            end
+            player_resolver()
+            @player_move['new_move'] = false
+          end
+      end
+    end
+
+    sockets = Thread.new 
+          server = TCPServer.new 6543
+          player_for_connection = @players[rand(@number_of_players)]
+          loop do
+            client = server.accept
+            input = server.gets
+            semaphore.synchronize
+              case input
+              when "connect"
+                @players.each do |p|
+                  client.puts p.player_name
+                end
+                client.puts "Your player is " + player_for_connection.player_name 
+              when "up"
+                if player_for_connection.position.row != 0
+                  player_for_connection.position.row -= 1
+                  client.puts player_for_connection.position
+                end
+              when "down"
+                if player_for_connection.position.row != (@board_y - 1)
+                  player_for_connection.position.row += 1
+                  client.puts player_for_connection.position
+                end
+              when "left"
+                if player_for_connection.position.column != @playing_board.columns[0]
+                  player_for_connection.position.column = decrease_column()
+                  client.puts player_for_connection.position
+                end
+              when "right"
+                if player_for_connection.position.column != @playing_board.columns[-1]
+                  player_for_connection.position.column = increase_column()
+                  client.puts player_for_connection.position
+                end
+              when "exit"
+                break
+              else
+                client.puts "Invalid command"
+              end
+            end
+          end
+    end
+    
+  end
+
 end
 
-#m = Game.new
+m = Game.new
+m.run_game()
